00881c33392d7aac368b090743d6648ae547edcb
amalakamala <amalakamala.a@gmail.com>
2017-04-27 12:18:09 -0300

    Se agrego css e img, se realizaron cambios en js y en index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 //funcion que agrega texto desde el input a una lista, siendo visible esta en el html
 function nombreFuncion(){
 	var varInput = document.getElementById("ingreso"); 
-	var contListas = document.getElementsByClassName("listas");
+	var contListas = document.getElementsByClassName("listas"); 
 
-	contListas[0].innerHTML += "<li>"+ varInput.value + "</li>" + "<br>"; 
-
-	document.getElementById("ingreso").value = " "; 
-	//Tambien se puede ingresar varInput.value = " ";
+	if(varInput.value == undefined || varInput.value == ""){
+		alert("Ingresa un elemento a la lista");
+	}else{
+		contListas[0].innerHTML += "<li>"+ varInput.value + "</li>" + "<br>";
+		document.getElementById("ingreso").value = " "; 
+		//Tambien se puede ingresar varInput.value = " ";
+	}
 }
+
+
